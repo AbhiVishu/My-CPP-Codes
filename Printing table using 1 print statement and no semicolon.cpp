870602360164f81df650fb441c162c81265556e8
Abhishek Vishwakarma <abhishek.vishwakarma9@gmail.com>
2017-03-03 16:58:23 +0530

    Create Printing table using 1 print statement and no semicolon.cpp

---
+++ Printing table using 1 print statement and no semicolon.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main(int l,int n=1)
+{
+    if(n=1a){
+        if (scanf("%d", &l)) {}
+       while(n <= 10){
+        if (printf("%d\n", l*n++)) {}
+
+       }
+    }
+}
